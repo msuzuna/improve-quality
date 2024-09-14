57a2f03b1066bac483d92f544eb1412ccc301ed1
msuzuna <suzuna.matsuo@nijibox.co.jp>
2024-09-14 20:10:07 +0900

    change: 型の扱いを変更

--- src/js/modules/displayMoviePoster.js
+++ src/js/modules/displayMoviePoster.js
@@ -1,34 +1,54 @@
 import { fetchData } from "./fetch.js";
 
 /**
+ * @typedef {Object} Result
+ * @property {number} id
+ * @property {boolean} adult
+ * @property {string} backdrop_path
+ * @property {number[]} genre_ids
+ * @property {string} original_language
+ * @property {string} original_title
+ * @property {string} overview
+ * @property {number} popularity
+ * @property {string} poster_path
+ * @property {string} release_date
+ * @property {string} title
+ * @property {boolean} video
+ * @property {number} vote_average
+ * @property {number} vote_count
+ */
+
+/**
+ * @typedef {Object} MovieData
+ * @property {number} page
+ * @property {number} total_pages
+ * @property {number} total_results
+ * @property {Result[]} results
+ */
+
+/**
  * 映画APIを利用して現在放映されている映画のポスターを表示させる
- * @async
- * @function
- * @returns {void} 返り値なし
  */
 export const displayMoviePoster = async () => {
-  /** @type {HTMLButtonElement | null} */
   const observationTrigger = document.querySelector(
     "[data-modal-open='movie-poster']",
   );
-  /** @type {boolean | null} */
+  if (!(observationTrigger instanceof HTMLButtonElement)) return;
+
   const isButtonDisabled = observationTrigger?.disabled;
   // モーダルを開くボタンとモーダルの中身のidが一致しない場合モーダルが開かないため、早期リターン
   if (!(observationTrigger instanceof HTMLButtonElement) || isButtonDisabled)
     return;
 
-  /** @type {HTMLElement | null} */
   const posterBlock = document.querySelector("[data-movie='block']");
   if (!(posterBlock instanceof HTMLElement)) {
     observationTrigger.disabled = true;
     return;
   }
 
-  /** @type {string} */
   const url = "https://getmovieinformation-afq4w33w3q-uc.a.run.app/";
-  /** @type {Object} 映画情報が格納されたオブジェクト */
+  /** @type {MovieData} 映画情報が格納されたオブジェクト */
   const defaultData = await fetchData(url);
-  /** @type {{ page: number | undefined, total_pages: number | undefined}} */
   const { page: defaultPage, total_pages: totalPages } = defaultData;
   // 通信がうまくいかなかったなど、必要な情報が得られなかった場合は早期リターン
   if (defaultPage === undefined || totalPages === undefined) {
@@ -38,26 +58,19 @@ export const displayMoviePoster = async () => {
 
   /**
    * 映画のポスターを取得し、映画ポスター要素を生成する関数
-   * @async
-   * @function
-   * @param {number} pageIndex
-   * @returns {void}
    */
   const createPosterElement = async (pageIndex = 1) => {
-    /** @type {Object} 映画情報が格納されたオブジェクト */
+    /** @type {MovieData} 映画情報が格納されたオブジェクト */
     const data = await fetchData(`${url}?page=${pageIndex}`);
-    /** @type {{results: Array<Object> | undefined}} */
     const { results } = data;
     if (results === undefined) {
       observationTrigger.disabled = true;
       return;
     }
 
-    /** @type {DocumentFragment} */
     const fragment = new DocumentFragment();
 
     results.forEach((result) => {
-      /** @type {{poster_path: string | null, title: string}} */
       const { poster_path, title } = result;
       if (poster_path === null) return;
       const srcUrl = `https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path}`;
@@ -76,15 +89,11 @@ export const displayMoviePoster = async () => {
 
   /**
    * 監視を開始する関数
-   * @function
    * @param {number} defaultPageIndex
    * @param {number} totalPageIndex
-   * @returns {void}
    */
   const startObservation = (defaultPageIndex, totalPageIndex) => {
-    /** @type {HTMLElement | null} */
     const scrollArea = document.getElementById("movie-poster");
-    /** @type {HTMLElement | null} */
     const target = document.querySelector("[data-movie='end']");
 
     /**
@@ -97,26 +106,16 @@ export const displayMoviePoster = async () => {
       return;
     }
 
-    /** @type {{root: HTMLElement, threshold: number}} */
-    const options = {
-      root: scrollArea,
-      threshold: 0,
-    };
-
     /**
      * 次のページ数を取得する関数を生成する関数
-     * @function
      * @param {number} defaultPageIndex
      * @param {number} totalPageIndex
-     * @returns {Function | undefined} 次のページ数を取得する関数
      */
     const getNextIndexFactory = (defaultPageIndex, totalPageIndex) => {
       let currentPageIndex = defaultPageIndex;
 
       /**
        * 次のページ数を取得する
-       * @function
-       * @returns {number}
        */
       const getNextIndex = () => {
         if (currentPageIndex === totalPageIndex) {
@@ -129,27 +128,20 @@ export const displayMoviePoster = async () => {
       return getNextIndex;
     };
 
-    /** @type {Function} */
     const nextIndex = getNextIndexFactory(defaultPageIndex, totalPageIndex);
 
-    /**
-     * オブザーバーの引数に設定するコールバック関数
-     * @function
-     * @param {IntersectionObserverEntry} entries
-     * @returns {void}
-     */
-    const infiniteScroll = (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          createPosterElement(nextIndex());
-        }
-      });
-    };
-
-    /** @type {IntersectionObserver} */
     const intersectionObserver = new IntersectionObserver(
-      infiniteScroll,
-      options,
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            createPosterElement(nextIndex());
+          }
+        });
+      },
+      {
+        root: scrollArea,
+        threshold: 0
+      },
     );
 
     observationTrigger.addEventListener("click", () => {
