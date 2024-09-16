a2c592a9c5edbb9a38af23d26c4827a620d7bbc4
msuzuna <suzuna.matsuo@nijibox.co.jp>
2024-09-17 00:55:32 +0900

    add: ローカルストレージ登録機能を追加

---
+++ src/js/modules/registerToLocalStorage.js
@@ -0,0 +1,52 @@
+export const registerToLocalStorage = () => {
+  const requestButtons = document.querySelectorAll("[data-request]");
+
+  requestButtons.forEach((requestButton) => {
+    const value = requestButton.getAttribute("data-request");
+    if (value === null) return;
+
+    const requireElementArray = [
+      ...document.querySelectorAll(`[data-request-${value}]`),
+    ];
+
+    const requiredValueArray = requireElementArray.map((item) => {
+      const targetAttr = item.attributes.getNamedItem(`data-request-${value}`);
+      if (targetAttr === null) return;
+      return targetAttr.value;
+    });
+
+    requiredValueArray.forEach((requiredValue) => {
+      if (requiredValue === undefined) return;
+      const triggerElement = document.querySelector(
+        `[data-trigger-${requiredValue}]`,
+      );
+      if (!(triggerElement instanceof HTMLMenuElement)) return;
+
+      /** @type {string | null} */
+      const triggerValue = triggerElement.getAttribute(
+        `data-trigger-${requiredValue}`,
+      );
+      if (triggerValue === null) return;
+
+      const triggerInputs = document.getElementsByName(triggerValue);
+      triggerInputs.forEach((triggerInput) => {
+        triggerInput.addEventListener("change", (e) => {
+          const { currentTarget } = e;
+          if (!(currentTarget instanceof HTMLInputElement)) return;
+          const { name, value } = currentTarget;
+          localStorage.setItem(name, value);
+
+          const requiredInputs = document.getElementsByName(requiredValue);
+          requiredInputs.forEach((requiredInput) => {
+            requiredInput.addEventListener("change", (e) => {
+              const { currentTarget } = e;
+              if (!(currentTarget instanceof HTMLInputElement)) return;
+              const { name, value } = currentTarget;
+              localStorage.setItem(name, value);
+            });
+          });
+        });
+      });
+    });
+  });
+};
